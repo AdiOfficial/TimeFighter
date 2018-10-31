9e878a01113cd7921f53f187119e7323bc8c2596
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-31 12:11:34 +0200

    Set onClickListener and implemented incrementScore method

--- app/src/main/java/ws/tilda/anastasiia/timefighter/MainActivity.kt
+++ app/src/main/java/ws/tilda/anastasiia/timefighter/MainActivity.kt
@@ -9,13 +9,27 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
+    internal var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tapMeButton = findViewById<Button>(R.id.tap_me_button)
-        gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
-        timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+
+        tapMeButton.setOnClickListener{
+            view ->
+            incrementScore()
+        }
     }
+
+    private fun incrementScore() {
+        score += 1
+        val newScore = getString(R.string.score_text_view_name, score.toString())
+        gameScoreTextView.text = newScore
+    }
+
+
 }
