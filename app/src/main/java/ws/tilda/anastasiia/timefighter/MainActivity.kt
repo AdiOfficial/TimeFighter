62b2f4e825419feeeaa9e52c8eebd8c7359fa15b
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-31 17:59:35 +0200

    Inflated the menu in main activity

--- app/src/main/java/ws/tilda/anastasiia/timefighter/MainActivity.kt
+++ app/src/main/java/ws/tilda/anastasiia/timefighter/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Menu
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -73,6 +74,12 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
 
         Log.d(TAG, "onDestroy called.")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 
     private fun resetGame() {
